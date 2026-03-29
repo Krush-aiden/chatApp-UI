f470a7ef14302a266af54fde0ade59f653ec4ae8
Krush-aiden <krushnamahapatra8@gmail.com>
2026-03-29 20:44:15 +0530

    feat: use env vars for API URL (dev/prod toggle)

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,14 @@
 import { io } from "socket.io-client";
 
-const SERVER_URL = "http://localhost:5000";
+const isProd = import.meta.env.VITE_ENVIRONMENT === "prod";
+
+const SERVER_URL = isProd
+  ? import.meta.env.VITE_BACKEND_USER_API_URL_PROD
+  : import.meta.env.VITE_BACKEND_USER_API_URL_DEV;
 
 export const socket = io(SERVER_URL, {
   autoConnect: false,
   transports: ["websocket"],
 });
 
-export const API_URL = "";
+export const API_URL = SERVER_URL;
